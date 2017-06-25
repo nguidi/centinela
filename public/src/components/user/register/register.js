fc03edc4f948ee6266434d0bb5a32dc10ae35bde
Neri Guidi <neri.guidi@gmail.com>
2017-06-25 04:45:25 -0300

    Formulario de registro

--- public/src/components/user/register/register.js
+++ public/src/components/user/register/register.js
@@ -1,19 +1,62 @@
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
+import route from 'can-route';
 import './register.less';
 import view from './register.stache';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap-datepicker';
 import 'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css';
 
-export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the user-register component'
-  }
-});
+export const ViewModel
+=	DefineMap
+		.extend(
+			{
+				nextStep: function()
+				{
+					var $active = $('.wizard .nav-tabs li.active');
+					$active.next().removeClass('disabled');
+					$active.next().find('a[data-toggle="tab"]').click();
+				}
+			,	prevStep: function()
+				{
+					var $active = $('.wizard .nav-tabs li.active');
+					$active.prev().find('a[data-toggle="tab"]').click();
+				}
+			,	register: function()
+				{
+					$('.panel-wrapper')
+						.addClass('wd-xl')
+						.removeClass('wd-xlg');
+					$('.wizard').hide()
+					$('.register-success').show()
+				}
+			}
+		);
 
-export default Component.extend({
-  tag: 'user-register',
-  ViewModel,
-  view
-});
+export default Component.extend(
+	{
+		tag: 'user-register'
+	,	ViewModel
+	,	view
+	,	events:
+		{
+			inserted: function()
+			{
+			//Wizard
+			$('a[data-toggle="tab"]')
+				.on(
+					'show.bs.tab'
+				,	function(e)
+					{
+
+						var $target = $(e.target);
+
+						if ($target.parent().hasClass('disabled')) {
+							return false;
+						}
+					}
+				);
+			}
+		}
+	}
+);
